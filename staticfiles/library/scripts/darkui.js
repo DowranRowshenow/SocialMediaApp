4917174277db89b0a3d8515332e6bd1bbe4c1dbd
DowranRowshenow <dowranrowshenow3d@gmail.com>
2025-07-04 12:34:13 +0500

    .gitignore

--- staticfiles/library/scripts/darkui.js
+++
@@ -1,6 +0,0 @@
-function CopyClipboard(element) 
-{
-    element.select();
-    element.setSelectionRange(0, 99999);
-    navigator.clipboard.writeText(element.value);
-}
--- staticfiles/scripts/notifications.js
+++
@@ -1,7 +0,0 @@
-const notificationSocket = new WebSocket('ws://' + window.location.host + '/ws/notifications/' + userName + '/');
-
-/* WEBSOCKETS */
-notificationSocket.onmessage = function(e)
-{
-    const data = JSON.parse(e.data);
-}
